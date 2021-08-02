19ac7c0192eb4fa3fbdfbce4f734ba1188937743
AustinthePoop <87739694+AustinthePoop@users.noreply.github.com>
2021-08-03 03:51:47 +1000

    Create chat bot.py

---
+++ chat bot.py
@@ -0,0 +1,8 @@
+from prsaw import RandomStuffV2
+#putt different thing in this chat to make ai different# 
+rs = RandomStuffV2()
+response = rs.get_ai_response("tell me a joke")
+print(response)
+
+rs.close()
+#pip install prsaw and randomstuff
