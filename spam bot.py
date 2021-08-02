5eca0d7ad918c5c32b3ac74a67eafa374132d687
AustinthePoop <87739694+AustinthePoop@users.noreply.github.com>
2021-08-03 03:44:40 +1000

    Create spam bot.py

---
+++ spam bot.py
@@ -0,0 +1,9 @@
+import pyautogui as auto
+from time import sleep
+
+while True:
+    auto.write('you online :)')
+    auto.press('enter')
+    sleep(1)
+
+#will have to pip install pyautogui and time
